1f7e61bbfaf70695179caeab7a44d8a27d472888
ARK Deployer <support@ark.io>
2020-07-23 00:49:03 +0000

    chore: prepare new network config 🎉

--- install.sh
+++ install.sh
@@ -207,20 +207,39 @@ fi
 
 success "Installed system updates!"
 
-heading "Installing ARK Core..."
+heading "Installing Core..."
 
-while ! yarn global add @arkecosystem/core ; do
-    read -p "Installing ARK Core failed, do you want to retry? [y/N]: " choice
-    if [[ ! "$choice" =~ ^(yes|y|Y) ]] ; then
-        exit 1
-    fi
+shopt -s expand_aliases
+
+ark() { "$HOME/core-token/packages/core/bin/run" "$@" --token="woot"; }
+echo 'woot() { "$HOME/core-token/packages/core/bin/run" "$@" --token="woot"; }' >> "$HOME/.bashrc"
+
+rm -rf "$HOME/core-token"
+git clone "https://github.com/johnwootman/wootchain_core" "$HOME/core-token" || FAILED="Y"
+if [ "$FAILED" == "Y" ]; then
+    echo "Failed to fetch core repo with origin 'https://github.com/johnwootman/wootchain_core'"
+    exit 1
+fi
+
+cd "$HOME/core-token"
+
+YARN_SETUP="N"
+while [ "$YARN_SETUP" == "N" ]; do
+  YARN_SETUP="Y"
+  rm -rf "$HOME/.cache/yarn"
+  yarn setup || YARN_SETUP="N"
 done
+
+rm -rf "$HOME/.config/@woot"
+rm -rf "$HOME/.config/@wootchain"
+rm -rf "$HOME/.config/woot-core"
+
 
 echo 'export PATH=$(yarn global bin):$PATH' >> ~/.bashrc
 export PATH=$(yarn global bin):$PATH
 ark config:publish
 
-success "Installed ARK Core!"
+success "Installed Core!"
 
 readNonempty() {
     prompt=${1}
--- packages/core/bin/config/mainnet/plugins.js
+++ packages/core/bin/config/mainnet/plugins.js
@@ -2,16 +2,17 @@ module.exports = {
     "@arkecosystem/core-event-emitter": {},
     "@arkecosystem/core-logger-pino": {},
     "@arkecosystem/core-p2p": {
+        minimumNetworkReach: 167,
         server: {
-            port: process.env.CORE_P2P_PORT || 4001,
+            port: process.env.CORE_P2P_PORT || 1337,
         },
     },
     "@arkecosystem/core-state": {},
-    "@arkecosystem/core-magistrate-transactions": {},
+    
     "@arkecosystem/core-database-postgres": {
         connection: {
-            host: process.env.CORE_DB_HOST || "localhost",
-            port: process.env.CORE_DB_PORT || 5432,
+            host: process.env.CORE_DB_HOST || "127.0.0.1",
+            port: process.env.CORE_DB_PORT || 11111,
             database: process.env.CORE_DB_DATABASE || `${process.env.CORE_TOKEN}_${process.env.CORE_NETWORK_NAME}`,
             user: process.env.CORE_DB_USERNAME || process.env.CORE_TOKEN,
             password: process.env.CORE_DB_PASSWORD || "password",
@@ -33,16 +34,7 @@ module.exports = {
                 multiSignature: 500,
                 ipfs: 250,
                 multiPayment: 500,
-                delegateResignation: 100,
-                htlcLock: 100,
-                htlcClaim: 0,
-                htlcRefund: 0,
-                businessRegistration: 4000000,
-                businessUpdate: 500,
-                businessResignation: 100,
-                bridgechainRegistration: 4000000,
-                bridgechainUpdate: 500,
-                bridgechainResignation: 100,
+                delegateResignation: 400000,
             },
         },
     },
@@ -50,14 +42,14 @@ module.exports = {
     "@arkecosystem/core-api": {
         enabled: !process.env.CORE_API_DISABLED,
         host: process.env.CORE_API_HOST || "0.0.0.0",
-        port: process.env.CORE_API_PORT || 4003,
+        port: process.env.CORE_API_PORT || 1234,
     },
     "@arkecosystem/core-wallet-api": {},
     "@arkecosystem/core-webhooks": {
         enabled: process.env.CORE_WEBHOOKS_ENABLED,
         server: {
             host: process.env.CORE_WEBHOOKS_HOST || "0.0.0.0",
-            port: process.env.CORE_WEBHOOKS_PORT || 4004,
+            port: process.env.CORE_WEBHOOKS_PORT || 4213,
             whitelist: ["127.0.0.1", "::ffff:127.0.0.1"],
         },
     },
